58e0df1f1a850c72fd22b16579102de9817e4401
Ye Wenyu <yewenyu@bupt.edu.cn>
2026-04-15 21:59:10 +0800

    update

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -10,6 +10,7 @@
 
 // I AM NOT DONE
 
+use std::mem::replace;
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -24,14 +25,26 @@ impl ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError{
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    let x = s.parse::<i64>();
+    if let Err(e) = x {
+        return Err(ParsePosNonzeroError::ParseInt(e));
+    }
+
+    if let Ok(x) = x {
+        match PositiveNonzeroInteger::new(x) {
+            Ok(x) => Ok(x),
+            Err(e) => Err(ParsePosNonzeroError::Creation(e))
+        };
+    }
+    Ok(PositiveNonzeroInteger(0))
 }
 
 // Don't change anything below this line.
