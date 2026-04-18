59fef489b2b58b48640bb1e2aec12a860bf5b4ab
Ye Wenyu <yewenyu@bupt.edu.cn>
2026-04-18 14:33:27 +0800

    update

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -8,8 +8,6 @@
 // need to be modified.
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -32,7 +30,9 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    let mut result = 0;
+    let _ :Vec<_> = map.iter().map(|x| {if *x.1 == value {result+=1}}).collect();
+    result
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,10 +48,9 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
 }
 
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
-    // collection is a slice of hashmaps.
-    // collection = [{ "variables1": Complete, "from_str": None, ... },
-    //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let mut ret = 0;
+    let _ :Vec<_>=collection.iter().map(|x| {ret+=count_iterator(x,value)}).collect();
+    ret
 }
 
 #[cfg(test)]
